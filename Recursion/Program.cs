7d6c2d19bbf1b0110ce3b1c7c005487747f43c83
Reham135 <reham7166@gmail.com>
2023-11-02 20:53:33 +0200

    Add project files.

---
+++ Recursion/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Recursion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Recursion.DrawPyramid(4);
+        }
+    }
+}
+
+
+
+//output
+//*
+//**
+//***
+//****
---
+++ Recursion/Recursion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion;
+
+public static class Recursion
+{
+    public static void DrawPyramid(int level)
+    {
+        if (level <= 0) return;          //to avoid infinity loop that leads to stack overflow
+        DrawPyramid(level - 1);          //calling the function inside itself that is called recursion
+
+        for (int i = 0; i < level; i++)
+        {
+            Console.Write("*");
+
+        }
+        Console.WriteLine("");
+    }
+
+}
